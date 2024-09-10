6296229bd0347b028d14298cc13a1d0917a080cf
Moncii <yoshiroberts@proton.me>
2024-09-10 19:02:15 +0000

    Raylib now gets compiled and linked.
    Updated the readme.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Project Tower Defense.
+# Project Tower Defense.
+
+A hybrid tower defense and typing game.
+
+## Building
+
+Install [xmake](https://xmake.io/#/guide/installation)
+
+If compiling on linux make sure you have installed the required [dependencies](https://github.com/raysan5/raylib/wiki/Working-on-GNU-Linux) for Linux.
--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,25 @@
 add_rules("mode.debug", "mode.release")
 
+package("raylib")
+add_deps("cmake")
+set_sourcedir(path.join(os.scriptdir(), "lib/raylib"))
+on_install(function(package)
+	local configs = {}
+	-- table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+	table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+	import("package.tools.cmake").install(package, configs)
+end)
+-- on_test(function (package)
+--     assert(package:has_cfuncs("add", {includes = "foo.h"}))
+-- end)
+package_end()
+
+add_requires("raylib")
+
 target("twd")
-    set_kind("binary")
-    add_files("src/*.c")
+set_kind("binary")
+add_files("src/*.c")
+add_packages("raylib")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
@@ -72,4 +89,3 @@ target("twd")
 --
 -- @endcode
 --
-
