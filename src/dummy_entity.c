1f087efa22734e770bee0188700bd34f1fcf27ac
Moncii <yoshiroberts@proton.me>
2024-09-13 17:54:30 +0000

    Added a dummy entity type for reference purposes.

---
+++ src/dummy_entity.c
@@ -0,0 +1,16 @@
+#ifndef DUMMY_ENTITY_H
+#define DUMMY_ENTITY_H
+
+// A dummy entity for reference purposes.
+typedef struct {
+
+	// Position.
+	int x;
+	int y;
+
+	// Other fields and etc.
+	int health;
+
+} DummyEntity;
+
+#endif
