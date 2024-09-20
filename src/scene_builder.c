6b494f8fc032fcd9c03e5debaf4ee75eeae4f014
ChuppN <chuppnat@gmail.com>
2024-09-19 20:53:40 -0400

    added partial functionality for filling the scene with tiles

--- src/scene_builder.c
+++ src/scene_builder.c
@@ -1,5 +1,10 @@
+#define MAXWIDTH=128
+#define MAXHEIGHT=128
+
 #ifndef SCENE_BUILDER_H
 #define SCENE_BUILDER_H
+#endif
+
 
 typedef struct {
 	Asset* tileset;
@@ -17,12 +22,23 @@ void scene_initialize(int difficulty, int waves) {
 	Scene newScene;
 	newScene.difficulty = difficulty;
 	newScene.waves = waves;
-
+	scene_populate(newScene);
 }
 
-void scene_populate()
+void scene_populate(Scene newScene)
 {
+int traverse = 0;
+for(int i; i<MAXHEIGHT; i++)
+	{
+	for(int j; j<MAXWIDTH; j++)
+		{
+		//Find value set at [i][j]
+		//go into assets folder and yoink image
+		//render image based on which part we want
+	
 
+		}
+	}
 
 
 
