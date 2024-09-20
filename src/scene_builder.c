43b0124079520edef2d6197516d0177cd49b75db
ChuppN <chuppnat@gmail.com>
2024-09-20 10:11:59 -0400

    Added near full functionality to scene builder for terrain features ground and water.

--- src/scene_builder.c
+++ src/scene_builder.c
@@ -1,14 +1,39 @@
-#define MAXWIDTH=128
-#define MAXHEIGHT=128
+#include "assets.h"
+
+#define MAXWIDTH 128
+#define MAXHEIGHT 128
 
 #ifndef SCENE_BUILDER_H
 #define SCENE_BUILDER_H
 #endif
 
 
+enum tileTypes {
+    GROUND_FLAT = 1,
+    GROUND_ELEVATE = 2,
+    GROUND_SHADOW = 3,
+    WATER = 4,
+    WATER_ROCK1 = 5,
+    WATER_ROCK2 = 6,
+    WATER_ROCK3 = 7,
+	WATER_ROCK4 = 8
+};
+
+
+const char* tilePaths[] = {
+	"",
+    "assets/Terrain/Ground/Tilemap_Flat.png",
+    "assets/Terrain/Ground/Tilemap_Elevation.png",
+    "assets/Terrain/Ground/Shadows.png",
+    "assets/Terrain/Water/Water.png",
+    "assets/Terrain/Water/Rocks/Rocks_01.png",
+    "assets/Terrain/Water/Rocks/Rocks_02.png",
+    "assets/Terrain/Water/Rocks/Rocks_03.png",
+	"assets/Terrain/Water/Rocks/Rocks_04.png"
+};
+
 typedef struct {
-	Asset* tileset;
-	int tile_size;
+	int *AssetPtr;
 	int tiles[128][128];
 } Tilemap ;
 
@@ -18,29 +43,30 @@ typedef struct {
 	Tilemap tilemap;
 } Scene;
 
+//================================================================================
+
+const char[] getTilePath(int tileType) {
+    return tilePaths[tileType];
+}
+
+//================================================================================
+
 void scene_initialize(int difficulty, int waves) {
 	Scene newScene;
 	newScene.difficulty = difficulty;
-	newScene.waves = waves;
+	newScene.waves = waves;f
 	scene_populate(newScene);
 }
-
+//================================================================================
 void scene_populate(Scene newScene)
 {
-int traverse = 0;
-for(int i; i<MAXHEIGHT; i++)
+for(int i=0; i < MAXHEIGHT; i++)
 	{
-	for(int j; j<MAXWIDTH; j++)
+	for(int j=0; j < MAXWIDTH; j++)
 		{
-		//Find value set at [i][j]
-		//go into assets folder and yoink image
-		//render image based on which part we want
-	
-
+		newScene.tilemap.AssetPtr = assets_get(getTilePath(newScene.tilemap.tiles[i][j]));
+		DrawTexture(newScene.tilemap.AssetPtr->data.texture, i, j, WHITE);
 		}
 	}
-
-
-
-
 }
+//================================================================================
