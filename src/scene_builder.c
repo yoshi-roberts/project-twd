27fbd431f61a535731b9b52c03d723754a351d64
ChuppN <chuppnat@gmail.com>
2024-09-20 14:10:33 -0400

    Added header file and fixed small naming conventions. Removed waves modifier.

--- src/scene_builder.c
+++ src/scene_builder.c
@@ -1,31 +1,11 @@
 #include <stdbool.h>
 #include "../lib/raylib/src/raylib.h"
+#include "scene_builder.h"
 #include "game.h"
 #include "assets.h"
 #include "log.h"
 
-#define MAXWIDTH 128
-#define MAXHEIGHT 128
-
-#ifndef SCENE_BUILDER_H
-#define SCENE_BUILDER_H
-#endif
-
-
-enum tileTypes {
-    GROUND_FLAT = 1,
-    GROUND_ELEVATE = 2,
-    GROUND_SHADOW = 3,
-    WATER = 4,
-    WATER_ROCK1 = 5,
-    WATER_ROCK2 = 6,
-    WATER_ROCK3 = 7,
-	WATER_ROCK4 = 8,
-	BRIDGE = 9;
-};
-
-
-const char* tilePaths[] = {
+const char* TilePaths[] = {
 	"",
     "assets/Terrain/Ground/Tilemap_Flat.png",
     "assets/Terrain/Ground/Tilemap_Elevation.png",
@@ -38,48 +18,29 @@ const char* tilePaths[] = {
 	"assets/Terrain/Bridge/Bridge_All.png"
 };
 
-typedef struct {
-	int *AssetPtr;
-	int tiles[128][128];
-} Tilemap ;
 
-typedef struct {
-	int difficulty;
-	int waves;
-	Tilemap tilemap;
-} Scene;
-
-//================================================================================
-const char[] getTileFilePath(int tileType) {
-    return tilePaths[tileType];
+const char* Get_Tile_File_Path(int TileType) {
+    return TilePaths[TileType];
 }
-//================================================================================
-void pull_scene_tiles(){
 
 
-
-
-
+void scene_initialize(int difficulty) {
+	Scene NewScene;
+	NewScene.difficulty = difficulty;
+	scene_populate(NewScene);
 }
-//================================================================================
-void scene_initialize(int difficulty, int waves) {
-	Scene newScene;
-	newScene.difficulty = difficulty;
-	newScene.waves = waves;f
-	scene_populate(newScene);
-}
-//================================================================================
-void scene_populate(Scene newScene)
+
+
+void scene_populate(Scene NewScene)
 {
 for(int i=0; i < MAXHEIGHT; i++)
 	{
 	for(int j=0; j < MAXWIDTH; j++)
 		{
 		BeginDrawing();
-		newScene.tilemap.AssetPtr = assets_get(getTileFilePath(newScene.tilemap.tiles[i][j]));
-		DrawTexture(newScene.tilemap.AssetPtr->data.texture, i, j, WHITE);
+		NewScene.tilemap.AssetPtr = assets_get(Get_Tile_File_Path(NewScene.tilemap.tiles[i][j]));
+		DrawTexture(NewScene.tilemap.AssetPtr->data.texture, i, j, WHITE);
 		EndDrawing();
 		}
 	}
 }
-//================================================================================
