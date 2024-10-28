92116b6b21cc5b0ec29c46ca9fab3bf9eaab93b2
Moncii <yoshiroberts@proton.me>
2024-10-28 15:48:28 +0000

    Units now display their hp when selected.

--- src/unit.c
+++ src/unit.c
@@ -1,5 +1,7 @@
 #include "unit.h"
 #include "assets.h"
+#include <math.h>
+#include <stdio.h>
 
 Unit unit_new(UNIT_TYPE type, int x, int y) {
 	Unit unit = {};
@@ -27,12 +29,25 @@ Unit unit_new(UNIT_TYPE type, int x, int y) {
 
 void unit_draw(Unit *unit) {
 
+
+	asset_draw_tile(unit->asset, unit->type, unit->x * 16, unit->y * 16);
+
+
 	if (unit->selected) {
 		Color color = (Color){255, 75, 75, 128};
 		DrawCircleLines((unit->x * 16) + 8, (unit->y * 16) + 8, (unit->range * 16) + 8, color);
-	}
 
-	asset_draw_tile(unit->asset, unit->type, unit->x * 16, unit->y * 16);
+		char hp[128];
+		sprintf(hp, "HP: %d", unit->hp);
+
+		int size = MeasureText(hp, 10);
+		Vector2 pos = {
+			floor(((unit->x * 16) + 8) - (size / 2)),
+			floor((unit->y * 16) - 16),
+		};
+
+		DrawText(hp, pos.x, pos.y, 10, WHITE);
+	}
 
 	unit->selected = false;
 }
